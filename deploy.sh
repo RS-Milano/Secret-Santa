337605dbe53c2bcc40d075d6405736bf5515cd6c
rs-milano <zamurakin@gmail.com>
2025-11-30 13:23:55 +0300

    Fixed deploy.sh and docker-compose.yml

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,18 @@
 set -e
 PROJECT_DIR="/projects/Secret-Santa"
 cd "$PROJECT_DIR" || exit
+
+echo "Проверяем обновления в ветке main..."
 git fetch origin main
+
 LOCAL_HASH=$(git rev-parse HEAD)
 REMOTE_HASH=$(git rev-parse origin/main)
+
 if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+    echo "Обновления найдены. Пуллим и пересобираем контейнеры..."
     git pull origin main
-    docker-compose up -d --build
+    docker compose up -d --build
+    echo "Деплой завершён."
+else
+    echo "Обновлений нет. Деплой не требуется."
 fi
